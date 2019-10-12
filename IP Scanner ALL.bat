b565a1a5be1d888b588ddaba66ed7275d6118bf2
IWickGames <smarttecgaming@gmail.com>
2019-10-12 13:29:27 -0500

    Update IP Scanner ALL.bat
    This adds SafeMod that will attempt to stop computers from crashing by adding work delays to lower the hit amount on the CPU. The timer is the length of the delay that the SafeMod will hold the work load.

--- IP Scanner ALL.bat
+++ IP Scanner ALL.bat
@@ -1,5 +1,9 @@
 @echo off
 REM Created by @IWickGames
+REM SAVE MODE------[ON/OFF]--------------------
+set SAFEMODE=ON
+set SAFEMODE_TIMER=2
+REM -------------------------------------------
 set ran=%random%
 set /p enter="Set Start IP[y/n] "
 if %enter%==y goto setvars
@@ -11,13 +15,14 @@ set num4=1
 if %num4%==255 goto exit
 if %num3%==255 set num3=0 && set /a num4=%num4%+ 1
 if %num2%==255 set num2=0 && set /a num3=%num3%+ 1
-if %num1%==255 set num1=0 && set /a num2=%num2%+ 1
+if %num1%==255 set num1=0 && set /a num2=%num2%+ 1 & if %SAFEMOD%==ON timeout /nobreak %SAFEMODE_TIMER% >nul
 start "" /MIN "cmd /r ping -n 1 -w 1 %num4%.%num3%.%num2%.%num1%>nul && echo [%num4%.%num3%.%num2%.%num1%] PASS>>Scan%ran%.txt"
 echo Checking [%num4%.%num3%.%num2%.%num1%] ...
 ping -n 2 -w 1 127.0.0.1 >nul
 set /a num1=%num1%+ 1
 goto 1
 :exit
+echo.
 echo Done!
 pause >nul
 exit
