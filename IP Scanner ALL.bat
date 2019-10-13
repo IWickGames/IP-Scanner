440095df03391bc9ac279d3ae5462b341672ccfc
IWickGames <smarttecgaming@gmail.com>
2019-10-12 21:09:57 -0500

    Update IP Scanner ALL.bat

--- IP Scanner ALL.bat
+++ IP Scanner ALL.bat
@@ -7,6 +7,7 @@ REM -------------------------------------------
 set ran=%random%
 set /p enter="Set Start IP[y/n] "
 if %enter%==y goto setvars
+if %enter%==Y goto setvars
 set num1=0
 set num2=1
 set num3=1
@@ -15,12 +16,12 @@ set num4=1
 if %num4%==255 goto exit
 if %num3%==255 set num3=0 && set /a num4=%num4%+ 1
 if %num2%==255 set num2=0 && set /a num3=%num3%+ 1
-if %num1%==255 set num1=0 && set /a num2=%num2%+ 1 & if %SAFEMOD%==ON timeout /nobreak %SAFEMODE_TIMER% >nul
+if %num1%==255 set num1=0 && set /a num2=%num2%+ 1 & if "%SAFEMOD%"=="ON" timeout /nobreak %SAFEMODE_TIMER% >nul
 start "" /MIN "cmd /r ping -n 1 -w 1 %num4%.%num3%.%num2%.%num1%>nul && echo [%num4%.%num3%.%num2%.%num1%] PASS>>Scan%ran%.txt"
 echo Checking [%num4%.%num3%.%num2%.%num1%] ...
-ping -n 2 -w 1 127.0.0.1 >nul
-ping -n 2 -w 1 127.0.0.1 >nul
-ping -n 2 -w 1 127.0.0.1 >nul
+ping -n 1 -w 1 127.0.0.1 >nul
+ping -n 1 -w 1 127.0.0.1 >nul
+ping -n 1 -w 1 127.0.0.1 >nul
 set /a num1=%num1%+ 1
 goto 1
 :exit
@@ -35,5 +36,6 @@ set /p num2="Set IP[192.168.SET.%num1%]: "
 set /p num3="Set IP[192.SET.%num2%.%num1%]: "
 set /p num4="Set IP[SET.%num3%.%num2%.%num1%]: "
 set /p enter="Does this look correct[%num4%.%num3%.%num2%.%num1%][y/n] : "
-if %enter%==y goto 1 & cls
+if %enter%==y goto 1
+if %enter%==Y goto 1
 goto setvars
